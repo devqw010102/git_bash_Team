ce9700f60c95017dc65756d6cc9b8dd1ebf22d44
Jaydevlee <yousick5@gmail.com>
2025-12-11 11:17:58 +0900

    Modified testatest.html and testatest.js - issue #2

--- featA/featatest.js
+++ featA/featatest.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-    const tr_id = $('#id');  //아이디 입력창 지정
+    const id = $('#id');  //아이디 입력창 지정
     const resultId = $('#resultId');  // 유효성 결과 표시부분
     const idReg = /^(?=.*[a-zA-Z])(?=.*\d)[a-z|A-Z\d]{5,20}$/; //id정규식
     function idCheck(){
       if(idReg.test(id.val())){
-        resultId.html("");
-        resultId.css("color", "");
+        resultId.html("사용 가능한 아이디입니다.");
+        resultId.css("color", "green");
         return true;
       } else {
         resultId.html("아이디는 영문자와 숫자포함 5자이상 20자 이내여야 합니다.");
@@ -13,4 +13,12 @@ $(document).ready(function() {
         return false;
       }
     };
+
+    id.on('keyup', function() {
+            if(id.val() === "") {
+                resultId.html("");
+                return;
+            }
+            idCheck();
+        });
 });
